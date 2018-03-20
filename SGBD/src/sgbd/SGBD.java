f4089c3186864719537812df771d9224a9f54da9
Andres Saldaña Aguilar <pollyketpo@gmail.com>
2018-03-20 09:55:23 -0600

    modificacion a hola mundo

--- SGBD/src/sgbd/SGBD.java
+++ SGBD/src/sgbd/SGBD.java
@@ -10,7 +10,7 @@ public class SGBD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundote");
     }
     
 }
