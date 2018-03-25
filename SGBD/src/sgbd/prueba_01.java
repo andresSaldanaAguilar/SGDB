89909634cd7e66c65e197efcc9e3e430ed4e9611
@@NfLG273@@ <jesus_kdm@hotmail.com>
2018-03-25 10:18:14 -0600

    Prueba01 by Kaimorts

---
+++ SGBD/src/sgbd/prueba_01.java
@@ -0,0 +1,32 @@
+package sgbd;
+
+import java.util.Scanner;
+
+/*@author Kaimorts*/
+public class prueba_01 {
+    public static String name_BD="";
+    public static String name_TB="";
+    
+    public static void main(String[] args) {
+        Lexer a = new Lexer();
+        Queries dc = new Queries();
+        boolean continua=true;
+        String query="";
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.print("|-> ");
+            query = teclado.nextLine();
+            if (query.equals("exit") || query.equals("EXIT")){
+                continua = false; break;
+            }
+            if (query.contains("DATABASE") || 
+                query.contains("database")) {
+                name_BD = a.getNameDB(query);
+                dc.CREATE_DB(name_BD);
+                dc.SHOW_DATABASES();
+            }
+        } while (continua);
+        
+    }
+    
+}
